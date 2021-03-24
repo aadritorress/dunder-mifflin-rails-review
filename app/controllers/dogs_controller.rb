c3dbb01d2b890b49eb356ae5acae4b71e982f121
Adriana <adriana.amaral@msn.com>
2021-03-24 16:39:31 -0500

    completed

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,15 @@
 class DogsController < ApplicationController
+
+def index 
+    # @dogs = Dog.all
+    @dogs_sorted = Dog.all.sort_by{|o| o.employees.count}
+end 
+
+def show
+
+    @dog = Dog.find(params[:id])
+end
+
+
+
 end
